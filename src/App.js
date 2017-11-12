833f2ddd03f034dc220fdb9cf2104882d276fa97
João Victor <joaovpbarbosa@gmail.com>
2017-11-12 19:07:40 -0300

    Update project

--- src/App.js
+++ src/App.js
@@ -1,54 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
-import * as firebase from 'firebase';
+import APIConnection from './api/APIConnection.js'
 
-const config = {
-    apiKey: "AIzaSyD7kzni4vibnC1k3syUEpNa0DnCuLkeAuY",
-    authDomain: "login-note.firebaseapp.com",
-    databaseURL: "https://login-note.firebaseio.com",
-    projectId: "login-note",
-    storageBucket: "login-note.appspot.com",
-    messagingSenderId: "220195332405"
-  };
-  firebase.initializeApp(config);
+// const config = {
+//     apiKey: "AIzaSyD7kzni4vibnC1k3syUEpNa0DnCuLkeAuY",
+//     authDomain: "login-note.firebaseapp.com",
+//     databaseURL: "https://login-note.firebaseio.com",
+//     projectId: "login-note",
+//     storageBucket: "login-note.appspot.com",
+//     messagingSenderId: "220195332405"
+//   };
 
-
-
+// firebase.initializeApp(config);
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      user: "",
+      user: undefined,
       note: "",
       noteList: [],
     }
   }
-
+  // componentWillMount() {
+  //   firebase.auth().onAuthStateChanged((user) => {
+  //     this.setState({user: user});
+  //   });
+  // }
   handleNote = (event) => {
     this.setState({note: event.target.value});
   }
 
   loginUser = () =>{
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then((result) => {
-      let user = result.user;
-      this.setState({user: user});
-      firebase.database().ref('users/' + user.uid).update({
-        username: user.displayName,
-      });
+   APIConnection.loginUser(this.setState);
+    // APIConnection.teste();
+  }
+  // addNote = value => {
+  //   const noteRef = firebase.database().ref('notes/' + this.state.user.uid);
+  //   noteRef.push({
+  //     note: this.state.note
+  //   });
+  //   this.setState({note: ""});
+  // }
 
-    }).catch(function(error) {
-      let errorMessage = error.message;
-      console.log(errorMessage);
-    });
-  }
-  addNote = value => {
-    const noteRef = firebase.database().ref('notes/' + this.state.user.uid);
-    noteRef.push({
-      note: this.state.note
-    });
-    this.setState({note: ""});
-  }
   renderAddNote = () => (
     <div>
       <input value={this.state.note} onChange={this.handleNote}type="text" placeholder="Insira a nota"></input>
@@ -59,26 +52,26 @@ class App extends Component {
     
   )
 
-  myTodoList = () => {
-    let items = []
-    firebase.database().ref('/notes/' + this.state.user.uid).once('value').then((snapshot) => {
-      snapshot.forEach(function(childSnapshot) {
-        let item = childSnapshot.val();
-        item['.key'] = childSnapshot.key;
-        items.push(item);
-      });
-      this.setState({
-        noteList: items
-      });
-    });
-  }
+  // myTodoList = () => {
+  //   const items = []
+  //   firebase.database().ref('/notes/' + this.state.user.uid).once('value').then((snapshot) => {
+  //     snapshot.forEach(function(childSnapshot) {
+  //       const item = childSnapshot.val();
+  //       item['.key'] = childSnapshot.key;
+  //       items.push(item);
+  //     });
+  //     this.setState({
+  //       noteList: items
+  //     });
+  //   });
+  // }
   render() {
-    let loginButton = <button onClick={this.loginUser}>Login!</button>;
+    const loginButton = <button onClick={this.loginUser}>Login!</button>;
     return (
       <div className="App">
         <h1>{this.state.note}</h1>
-        <h1>{this.state.user === "" ? "Olá! Faça seu login!" : `Ola, ${this.state.user.displayName} como vai?`}</h1>
-        {this.state.user === "" ? loginButton : this.renderAddNote()}
+        <h1>{this.state.user === undefined ? "Olá! Faça seu logins!" : `Ola, ${this.state.user.displayName} como vai?`}</h1>
+        {this.state.user === undefined ? loginButton : this.renderAddNote()}
         <ul>
           {this.state.noteList.map((note, index) => (
             <li key={index}> {note.note}</li>
